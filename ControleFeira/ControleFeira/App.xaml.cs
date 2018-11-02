1d9b7ea9bcd0825ee400e2f28b9399ced7c59774
Caio <caiosant10@gmail.com>
2018-11-01 21:34:39 -0300

    Teste de inserção no banco SQlite. Inserindo a hora atual

---
+++ ControleFeira/ControleFeira/App.xaml.cs
@@ -0,0 +1,40 @@
+﻿using PCLExt.FileStorage;
+using PCLExt.FileStorage.Folders;
+using SQLite;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace ControleFeira
+{
+    public partial class App : Application
+    {
+        /* propriedade usada para acessar o banco de dados dentro da aplicação */
+        public SQLiteConnection Conexao { get; private set; }
+
+        public App()
+        {
+            /* Abrir a pasta local de arquivos e criar o arquivo banco.db. */
+            var pasta = new LocalRootFolder();
+            var arquivo = pasta.CreateFile("banco.db", CreationCollisionOption.OpenIfExists);
+
+            /* Criando conexao e depois tabela informacoes com os campos id e info */
+            Conexao = new SQLiteConnection(arquivo.Path);
+            Conexao.Execute("create table if not exists informacoes (id integer primary key autoincrement, info text)");
+
+            InitializeComponent();
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart() { }
+
+
+
+        protected override void OnSleep() { }
+        
+
+        protected override void OnResume() { }
+        
+    }
+}
---
+++ ControleFeira/ControleFeira/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ControleFeira
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = new Model.Model();
+            CarregarInformacoes();
+        }
+
+        /* Faz uma consulta na tabela informacoes do banco de dados, resgatando os dados que estão no campo info */
+        private void CarregarInformacoes()
+        {
+            var list = ((App)Application.Current).Conexao.Query<Model.Model>("select info from informacoes");
+            ListView.ItemsSource = list;
+        }
+
+        // Cria uma query insert para inserir o horário atual no banco de dados
+        private void Button_OnClicked(object sender, EventArgs e)
+        {
+            var query = $"insert into informacoes (info) values (&#039;{DateTime.Now:yyyy-MM-dd HH:mm:ss.ffff}&#039;)";
+            ((App)Application.Current).Conexao.Execute(query);
+            CarregarInformacoes();
+        }
+    }
+}
