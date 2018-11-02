494e618347ace36d45ec466aceb989a3d13c66f1
Caio <caiosant10@gmail.com>
2018-11-02 15:46:49 -0300

    correcao de interpolação de strings na insercao da data/hora atual no BD...

--- ControleFeira/ControleFeira/MainPage.xaml.cs
+++ ControleFeira/ControleFeira/MainPage.xaml.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
-
+using ControleFeira.ViewModel;
 namespace ControleFeira
 {
     public partial class MainPage : ContentPage
@@ -12,21 +12,21 @@ namespace ControleFeira
         public MainPage()
         {
             InitializeComponent();
-            BindingContext = new Model.Model();
             CarregarInformacoes();
         }
 
         /* Faz uma consulta na tabela informacoes do banco de dados, resgatando os dados que estão no campo info */
         private void CarregarInformacoes()
         {
-            var list = ((App)Application.Current).Conexao.Query<Model.Model>("select info from informacoes");
+            var list = ((App)Application.Current).Conexao.Query<Model>("select info from informacoes");
             ListView.ItemsSource = list;
         }
 
         // Cria uma query insert para inserir o horário atual no banco de dados
         private void Button_OnClicked(object sender, EventArgs e)
         {
-            var query = $"insert into informacoes (info) values (&#039;{DateTime.Now:yyyy-MM-dd HH:mm:ss.ffff}&#039;)";
+            var query = $"insert into informacoes (info) values ('{DateTime.Now:yyyy-MM-dd HH:mm:ss.ffff}')";
+
             ((App)Application.Current).Conexao.Execute(query);
             CarregarInformacoes();
         }
---
+++ ControleFeira/ControleFeira/ViewModel/Model.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ControleFeira.ViewModel
+{
+    class Model : MainViewModel
+    {
+        private string _info;
+
+        public string Info
+        {
+            get { return _info; }
+            set { SetProperty(ref _info, value); }
+        }
+    }
+}
