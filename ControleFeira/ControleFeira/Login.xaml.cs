9109ebff2b7d054ae7772a5e89afea94d83d14ea
Caio Santos <caiosant10@gmail.com>
2018-11-12 09:29:30 -0300

    tela de Login, navegacao entre telas de login, main e addItem

---
+++ ControleFeira/ControleFeira/Login.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ControleFeira
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Login : ContentPage
+	{
+		public Login ()
+		{
+			InitializeComponent ();
+		}
+
+        private void ButtonLogar_OnClicked(object sender, EventArgs e)
+        {
+            Navigation?.PushAsync(new MainPage());
+        }
+    }
+}
