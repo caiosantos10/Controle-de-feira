08bfe0ee9cfa02f2121a3fedd7c2770dcfbb97d1
Caio <caiosant10@gmail.com>
2018-11-23 14:48:52 -0200

    correcao da Bootstrapper.cs e LoginView criacao da LoginViewModel.

--- Controle de Feira/Views/LoginView.xaml.cs
+++ Controle de Feira/Views/LoginView.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Controle_de_Feira
+namespace Controle_de_Feira.Views
 {
     /// <summary>
-    /// Interação lógica para MainWindow.xam
+    /// Lógica interna para LoginView.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class LoginView : Window
     {
-        public MainWindow()
+        public LoginView()
         {
             InitializeComponent();
         }
