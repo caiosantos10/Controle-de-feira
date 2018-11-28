532919923bae6b00008c33311108dede40fbf738
Caio <caiosant10@gmail.com>
2018-11-28 21:47:15 -0200

    BD com entityframework e postgres, insert com Entidade Usuario funcionando

---
+++ ControleFeira/Models/ControleFeiraContext.cs
@@ -0,0 +1,20 @@
+﻿using ControleFeira.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControleFeira
+{
+    class ControleFeiraContext : DbContext
+    {
+        public ControleFeiraContext() : base("ControleFeira")
+        {
+            Database.SetInitializer<ControleFeiraContext>(new DropCreateDatabaseIfModelChanges<ControleFeiraContext>());
+        }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+    }
+}
---
+++ ControleFeira/Models/Usuario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControleFeira.Models
+{
+    class Usuario
+    {
+        public int UsuarioID { get; set; }
+        public string UsuarioName { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public byte[] Photo { get; set; }
+        public decimal Height { get; set; }
+        public float Weight { get; set; }
+    }
+}
