0a11ca603a732c8aa93f6196b9ccedc1bc2f5c2a
Caio <caiosant10@gmail.com>
2018-11-29 00:59:55 -0200

    materialize e caliburn

---
+++ ControleFeira/Bootstrapper.cs
@@ -0,0 +1,24 @@
+﻿using Caliburn.Micro;
+using ControleFeira.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ControleFeira
+{
+    class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<LoginViewModel>();
+        }
+    }
+}
